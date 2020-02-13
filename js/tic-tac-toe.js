8da9c3f4a20e2dc0f6d8ee0892e19bc5c8d1eb12
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2020-02-13 12:29:33 -0500

    idk

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,91 @@
+///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn;
+let win;
+let choose;
+let winX = 0;
+let winY = 0;
+
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");   // grab the subheader
+
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+document.getElementById("X-First").onclick = xTurn;
+document.getElementById("Y-First").onclick = oTurn;
+
+///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  turn = "X";
+  win = null;
+
+  render();
+}
+function xTurn() {
+  init();
+  document.getElementById("turn").innerHTML = "Turn: X"
+  turn = "X"
+}
+function oTurn() {
+  init();
+  document.getElementById("turn").innerHTML = "Turn: O"
+  turn = "O"
+}
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+}
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+}
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner ? winner : board.includes("") ? null : "T";
+}
